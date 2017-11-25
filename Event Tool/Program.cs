989a9c6d7912d1949e9bd638c82b39604b844cd7
kasey keahey <kasey_keahey@yahoo.com>
2017-11-25 15:13:13 -0600

    test

--- Event Tool/Program.cs
+++ Event Tool/Program.cs
@@ -9,7 +9,7 @@ namespace Ck2ModdingTool
     public static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application.test
         /// </summary>
 
         #region Vars
